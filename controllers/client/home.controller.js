4301f1bf04a854ff18d4f5f42aeb87171d864b53
quan <quan93909@gmail.com>
2025-08-15 19:59:31 +0700

    Làm trang danh sách sản phẩm bên Client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,3 +1,7 @@
-module.exports.index = (req, res) => {
-  res.render("client/pages/home/index");
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+  res.render("client/pages/home/index", {
+    pageTitle: "Trang chủ"
+  });
 }
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,7 +1,10 @@
 const homeRouter = require("./home.route");
+const productRouter = require("./product.route");
 
 module.exports = (app) => {
 
   app.use("/", homeRouter);
+
+  app.use("/products", productRouter);
   
 }
