b6fd8b0461934f71588b91ca862b4376643b063a
quan <quan93909@gmail.com>
2025-08-16 22:13:21 +0700

    Sắp xếp sản phẩm theo các tiêu chí khác nhau

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -34,8 +34,20 @@ module.exports.index = async (req, res) => {
     countProducts
   )
 
+  // End Pagination
+
+  // Sort
+  let sort = {};
+
+  if(req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  } else {
+    sort.position = "desc";
+  }
+  // End Sort
+
   const products = await Product.find(find)
-  .sort({ position: "desc" })
+  .sort(sort)
   .limit(objectPagination.limitItems)
   .skip(objectPagination.skip);
 
