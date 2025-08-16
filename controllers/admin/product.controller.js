ec03d15e8b8db8cd35bb70ae96b3a0bdc295c0c1
quan <quan93909@gmail.com>
2025-08-16 09:17:56 +0700

    Tối ưu Tính năng phân trang

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
 const searchHelper = require("../../helpers/search");
+const paginationHelper = require("../../helpers/pagination");
 
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
@@ -21,20 +22,16 @@ module.exports.index = async (req, res) => {
   }
 
   // Pagination
-  let objectPagination = {
-    currentPage: 1,
-    limitItems: 2
-  }
-
-  if(req.query.page) {
-    objectPagination.currentPage = parseInt(req.query.page);
-  }
-
-  objectPagination.skip = (objectPagination.currentPage - 1) * objectPagination.limitItems;
-
   const countProducts = await Product.countDocuments(find);
-  const totalPage = Math.ceil(countProducts/objectPagination.limitItems);
-  objectPagination.totalPage = totalPage;
+
+  let objectPagination = paginationHelper(
+    {
+      currentPage: 1,
+      limitItems: 2
+    },
+    req.query,
+    countProducts
+  )
 
   const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
 
