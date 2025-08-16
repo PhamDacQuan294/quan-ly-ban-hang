5e4635c31ddaa228b4983a0a398b41603ff5b8cd
quan <quan93909@gmail.com>
2025-08-16 13:56:12 +0700

    Tính năng thay đổi thứ tự sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -33,7 +33,10 @@ module.exports.index = async (req, res) => {
     countProducts
   )
 
-  const products = await Product.find(find).limit(objectPagination.limitItems).skip(objectPagination.skip);
+  const products = await Product.find(find)
+  .sort({ position: "desc" })
+  .limit(objectPagination.limitItems)
+  .skip(objectPagination.skip);
 
   res.render("admin/pages/products/index", {
     pageTitle: "Danh sách sản phẩm ",
@@ -77,6 +80,15 @@ module.exports.changeMulti = async (req, res) => {
         deleted: true,
         deletedAt: new Date()
       });
+      break;
+    case "change-position":
+      for (const item of ids) {
+        let [id, position] = item.split("-");
+        position = parseInt(position);
+        await Product.updateOne({ _id: id }, {
+          position: position
+        });
+      }
       break;
     default:
       break;
