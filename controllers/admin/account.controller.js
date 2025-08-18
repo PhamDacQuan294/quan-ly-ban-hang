ceea2fc8e5a068b2fce116f3d6f171910c2f4b62
quan <quan93909@gmail.com>
2025-08-18 14:39:46 +0700

    Cập nhật trang thái hoạt động hay dừng hoạt động

--- controllers/admin/account.controller.js
+++ controllers/admin/account.controller.js
@@ -27,6 +27,21 @@ module.exports.index = async (req, res) => {
   });
 }
 
+// [PATCH] /admin/products/change-status/:status/:id
+module.exports.changeStatus = async (req, res) => {
+  const status = req.params.status;
+  const id = req.params.id;
+
+  await Account.updateOne({
+    _id: id
+  }, {
+    status: status
+  });
+
+  req.flash("success", "Cập nhật trạng thái thành công!");
+
+  res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+}
 
 // [GET] /admin/accounts/create
 module.exports.create = async (req, res) => {
--- public/admin/js/account.js
+++ public/admin/js/account.js
@@ -23,3 +23,26 @@ if(buttonsDelete.length > 0) {
   })
 }
 // End delete item
+
+// Change status
+const buttonsChangeStatus = document.querySelectorAll("[button-change-status]");
+if(buttonsChangeStatus.length > 0) {
+  const formChangeStatus = document.querySelector("#form-change-status");
+  const path = formChangeStatus.getAttribute("data-path");
+
+  buttonsChangeStatus.forEach(button => {
+    button.addEventListener("click", () => {
+      const statusCurrent = button.getAttribute("data-status");
+      const id = button.getAttribute("data-id");
+
+      let statusChange = statusCurrent == "active" ? "inactive" : "active";
+
+      formChangeStatus.action = path + `/${statusChange}/${id}`;
+
+      formChangeStatus.action += `?_method=PATCH`;
+
+      formChangeStatus.submit();
+    })
+  })
+}
+// End change status
