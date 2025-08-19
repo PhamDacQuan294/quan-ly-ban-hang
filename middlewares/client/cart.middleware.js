dfc86aedf64ed943f85155ab8a02534cf00b0871
quan <quan93909@gmail.com>
2025-08-19 12:16:44 +0700

    Làm nút Mini Cart trên Header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -11,7 +11,13 @@ module.exports.cartId = async (req, res, next) => {
       expires: new Date(Date.now() + expiresCookie)
     });
   } else {
-    // Lay ra thoi
+    const cart = await Cart.findOne({
+      _id: req.cookies.cartId
+    });
+
+    cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
+
+    res.locals.miniCart = cart;
   }
 
   next();
