f09c0ce82074a7dc9d64a66ba182421e59d4cc35
quan <quan93909@gmail.com>
2025-08-19 12:48:47 +0700

    Cập nhật số lượng sản phẩm trong giỏ hàng

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -88,3 +88,26 @@ module.exports.delete = async (req, res) => {
 
   res.redirect("/cart")
 }
+
+// [GET] /update/:productId/:quantity
+module.exports.update = async (req, res) => {
+  const cartId = req.cookies.cartId;
+  const productId = req.params.productId;
+  const quantity = req.params.quantity;
+
+  await Cart.updateOne(
+    {
+      _id: cartId,
+      "products.product_id": productId
+    }, 
+    {
+      $set: {
+        "products.$.quantity": quantity
+      }
+    }
+  );
+
+  req.flash("success", "Cập nhật sản phẩm thành công!");
+
+  res.redirect("/cart")
+}
