7d6d038e2964948f0307a55930b702268142219e
quan <quan93909@gmail.com>
2025-08-20 08:27:57 +0700

    Lưu thêm user_id vào collection carts

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,6 +1,7 @@
 const md5 = require("md5");
 const User = require("../../models/user.model");
 const ForgotPassword = require("../../models/forgot-password.model");
+const Cart = require("../../models/cart.model");
 
 const sendMailHelper= require("../../helpers/sendMail");
 const generateHelper= require("../../helpers/generate");
@@ -71,6 +72,12 @@ module.exports.loginPost = async (req, res) => {
     return;
   }
 
+  await Cart.updateOne({
+    _id: req.cookies.cartId
+  }, {
+    user_id: user.id
+  });
+  
   res.cookie("tokenUser", user.tokenUser);
   
   res.redirect("/")
